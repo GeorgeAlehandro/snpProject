bd5dd82cb34fe2bf7b6021dd0916a162360c4f96
George Alehandro Saad <--global>
2022-09-29 12:26:00 +0200

    setup commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='snpProject',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='George Alehandro Saad',
+    author_email='',
+    description=''
+)
